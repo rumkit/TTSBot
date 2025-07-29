db170eb95109b1779d2e5a2699ebe2f35c17d93f
Anton Mitsengendler <anton.mitsengendler@outlook.com>
2025-07-29 17:50:34 +0200

    Initial commit

---
+++ TTSBot/Commands/CommandHandler.cs
@@ -0,0 +1,13 @@
+﻿public class UriHandler(HttpClient client)
+{
+    public HandlerResult Handle(string messageText)
+    {
+        if (Uri.TryCreate(messageText, UriKind.Absolute, out var inputUri))
+        {
+            return HandlerResult.Success();
+        }
+
+        return HandlerResult.Error(
+            "Blimey! That link be missin' or twisted like a kraken's tentacle. Toss me a proper magnet or webpage, lest ye be marooned!");
+    }
+}
---
+++ TTSBot/Commands/HandlerResult.cs
@@ -0,0 +1,5 @@
+﻿public record HandlerResult(bool IsSuccess, string ErrorMessage = "")
+{
+    public static HandlerResult Success() => new(IsSuccess: true);
+    public static HandlerResult Error(string errorMessage) => new(IsSuccess: false, errorMessage);
+}
